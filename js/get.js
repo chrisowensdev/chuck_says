fdaaf34a12cdfcd21473a57004e5001b1a08692c
Chris Owens <guitarrockstarco@gmail.com>
2020-09-14 16:53:44 -0400

    Added async await

--- js/get.js
+++ js/get.js
@@ -2,14 +2,21 @@
 
 function get(url) {
     // Step 1: Fetch the data
-    return fetch(url)
-        // Step 2: Run the json() method from the Response
-        .then(function (response) {
-            return response.json();
-        })
-        // Step 3: Return the data from the response.json metho
-        .then(function (data) {
-            return data;
-        })
-
+    return (
+        fetch(url)
+            // Step 2: Run the json() method from the Response
+            .then(function (response) {
+                return response.json();
+            })
+            // Step 3: Return the data from the response.json metho
+            .then(function (data) {
+                return data;
+            })
+    );
 }
+
+const getWithAwait = async (url) => {
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+};
--- js/scripts.js
+++ js/scripts.js
@@ -7,27 +7,37 @@ const modalOverlay = document.querySelector('.modal-overlay');
 const modalCloseButton = document.getElementById('closeModal');
 let category = 'dev';
 
-const getQuote = () => {
+const getQuote = async () => {
     const url = `https://api.chucknorris.io/jokes/random?category=${category}`;
-    get(url).then((fetchResponse) => {
-        chuckSays.innerHTML = fetchResponse.value;
-        modalOverlay.classList.toggle('open');
-    });
+    const theQuote = await getWithAwait(url);
+
+    chuckSays.innerHTML = theQuote.value;
+    modalOverlay.classList.toggle('open');
 };
 
-const getCategories = () => {
+const getCategories = async () => {
     const url = `https://api.chucknorris.io/jokes/categories`;
     const dropdownMenu = document.getElementById('categoryInput');
-    get(url).then((categoryArray) => {
-        categoryArray.map((category) => {
-            if (category !== 'explicit') {
-                const categoryOption = document.createElement('option');
-                categoryOption.value = category;
-                categoryOption.text = category;
-                dropdownMenu.append(categoryOption);
-            }
-        });
+    const categoryArray = await getWithAwait(url);
+    categoryArray.map((category) => {
+        if (category !== 'explicit') {
+            const categoryOption = document.createElement('option');
+            categoryOption.value = category;
+            categoryOption.text = category;
+            dropdownMenu.append(categoryOption);
+        }
     });
+
+    // get(url).then((categoryArray) => {
+    //     categoryArray.map((category) => {
+    //         if (category !== 'explicit') {
+    //             const categoryOption = document.createElement('option');
+    //             categoryOption.value = category;
+    //             categoryOption.text = category;
+    //             dropdownMenu.append(categoryOption);
+    //         }
+    //     });
+    // });
 };
 
 refreshQuote.addEventListener('click', (e) => {
